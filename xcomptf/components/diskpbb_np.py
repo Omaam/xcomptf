ee23b0b1cbca19b3b5dae68f3d4c960215100f5e
Omaam <omama.tomoki@gmail.com>
2023-08-06 18:48:01 +0900

    update for memory adoptation

--- xcomptf/components/diskpbb_np.py
+++ xcomptf/components/diskpbb_np.py
@@ -1,119 +1,131 @@
-"""dikspbb translated from FORTRAN code in Xspec.
+"""dikspbb translated from fortran code in xspec.
 
-This module is translated from ChatGPT3.5 in Aug. 5th, 2023
-by Tomoki Omama. Some modification and comment adding has been
+this module is translated from chatgpt3.5 in aug. 5th, 2023
+by tomoki omama. some modification and comment adding has been
 done to enhance the readibity.
-The user should be noted that this code is not well examined
+the user should be noted that this code is not well examined
 and thus, sometimes output wrong result from the original ones.
 """
 import numpy as np
 
 
-def dkbflx(TIN, p, E, dtype=np.float32):
+def dkbflx(tin, p, e, dtype=np.float32):
     """
-    Calculate the flux of the disk blackbody model at a given energy.
+    calculate the flux of the disk blackbody model at a given energy.
 
-    Args:
-        TIN (float): Inner temperature of the disk (keV).
-        p (float): Exponent for the temperature radial
-            dependence (T(r) ~ r^-p).
-        E (float): Energy (keV).
+    args:
+        tin (float): inner temperature of the disk (kev).
+        p (float): exponent for the temperature radial
+            dependence (t(r) ~ r^-p).
+        e (float): energy (kev).
 
-    Returns:
-        float: Photon flux in photons/s/cm^2/keV.
+    returns:
+        float: photon flux in photons/s/cm^2/kev.
     """
-    GAUSS = np.array(
+    gauss = np.array(
         [0.236926885, 0.478628670, 0.568888888, 0.478628670,
          0.236926885, -0.906179846, -0.538469310, 0.0,
          0.538469310, 0.906179846], dtype=dtype)
 
-    NNN = 10000
-    if E > TIN:
-        NNN = 5
-    elif E > 0.2 * TIN:
-        NNN = 10
-    elif E > 0.1 * TIN:
-        NNN = 100
-    elif E > 0.01 * TIN:
-        NNN = 500
-    elif E > 0.001 * TIN:
-        NNN = 1000
+    nnn = 10000
+    if e > tin:
+        nnn = 5
+    elif e > 0.2 * tin:
+        nnn = 10
+    elif e > 0.1 * tin:
+        nnn = 100
+    elif e > 0.01 * tin:
+        nnn = 500
+    elif e > 0.001 * tin:
+        nnn = 1000
 
-    XN = 1.0 / NNN / 2.0
-    PHOTON = 0.0
+    xn = 1.0 / nnn / 2.0
+    photon = 0.0
 
-    for i in range(1, NNN + 1):
-        XH = XN * (2 * i - 1)
+    for i in range(1, nnn + 1):
+        xh = xn * (2 * i - 1)
         for j in range(5):
-            X = XN * GAUSS[j + 5] + XH
-            if E / TIN / X >= 170.0:
-                DK = X ** (-2.0 / p - 1.0) * np.exp(-E / TIN / X)
+            x = xn * gauss[j + 5] + xh
+            if e / tin / x >= 170.0:
+                dk = x ** (-2.0 / p - 1.0) * np.exp(-e / tin / x)
             else:
-                DK = X ** (-2.0 / p - 1.0) / (np.exp(E / TIN / X) - 1.0)
+                dk = x ** (-2.0 / p - 1.0) / (np.exp(e / tin / x) - 1.0)
 
-            PHOTON += GAUSS[j] * DK * XN
+            photon += gauss[j] * dk * xn
 
-    PHOTON = 2.78e-3 * E * E * PHOTON * (0.75 / p)
+    photon = 2.78e-3 * e * e * photon * (0.75 / p)
 
-    return PHOTON
+    return photon
 
 
-def diskpbb(EAR, NE, PARAM, IFL, dtype=np.float32):
+def diskpbb(ear, ne, param, ifl, dtype=np.float32):
     """
-    Compute the multicolor disk blackbody model spectrum.
+    compute the multicolor disk blackbody model spectrum.
 
-    Args:
-        EAR (array-like): Energy bin edges (keV).
-        NE (int): Number of energy bins.
-        PARAM (array-like): Model parameters [TIN (keV), p].
-        IFL (int): Flag for error computation (not used in this model).
+    args:
+        ear (array-like): energy bin edges (kev).
+        ne (int): number of energy bins.
+        param (array-like): model parameters [tin (kev), p].
+        ifl (int): flag for error computation (not used in this model).
 
-    Returns:
-        tuple: Tuple containing arrays of photon counts in each energy
-            bin (PHOTAR) and errors (PHOTER).
+    returns:
+        tuple: tuple containing arrays of photon counts in each energy
+            bin (photar) and errors (photer).
     """
-    GAUSS = np.array(
+    gauss = np.array(
         [0.236926885, 0.478628670, 0.568888888, 0.478628670,
          0.236926885, -0.906179846, -0.538469310, 0.0,
          0.538469310, 0.906179846], dtype=dtype)
 
-    PHOTER = np.zeros(NE, dtype=dtype)
+    photer = np.zeros(ne, dtype=dtype)
 
-    TIN, p = PARAM
+    tin, p = param
 
-    EAR_shifted = EAR[1:]
-    EAR_prev = EAR[:-1]
-    XN = (EAR_shifted - EAR_prev) / 2.0
+    ear_shifted = ear[1:]
+    ear_prev = ear[:-1]
+    xn = (ear_shifted - ear_prev) / 2.0
 
-    PHOTAR = np.zeros(NE, dtype=dtype)
-    for i in range(NE):
-        XH = XN[i] + EAR_prev[i]
+    photar = np.zeros(ne, dtype=dtype)
+    for i in range(ne):
+        xh = xn[i] + ear_prev[i]
         photon = 0.0
         for j in range(5):
-            E = XN[i] * GAUSS[j + 5] + XH
-            photon += GAUSS[j] * dkbflx(TIN, p, E)
-        PHOTAR[i] = photon * XN[i]
+            e = xn[i] * gauss[j + 5] + xh
+            photon += gauss[j] * dkbflx(tin, p, e)
+        photar[i] = photon * xn[i]
 
-    return PHOTAR, PHOTER
+    return photar, photer
 
 
 def main():
-    NE = 3491                          # NE: Number of Energies?
-    EAR = np.linspace(0.1, 20., NE+1)  # EAR: Energy ARray?
-    PARAM = [0.2, 3/4]                 # [Tin, p]
-    IFL = 1                            # ???
+    ne = 3491                          # ne: number of energies?
+    ear = np.linspace(0.1, 20., ne+1)  # ear: energy array?
+    param = [0.2, 3/4]                 # [tin, p]
+    ifl = 1                            # ???
+
+    np.random.seed(1)
+    params = np.random.normal(loc=[0.2, 0.75], scale=[0.01, 0.0],
+                              size=(10000, 2))
+    print("parameter shape is {}".format(params.shape))
+    print(params)
 
     import time
+    from tqdm import tqdm
     t0 = time.time()
-    PHOTAR, PHOTER = diskpbb(EAR, NE, PARAM, IFL)
+    photars = []
+    for param in tqdm(params):
+        photar, photer = diskpbb(ear, ne, param, ifl)
+        photars.append(photar)
     t1 = time.time()
     print("ran time is {} s".format(t1-t0))
-    print(PHOTAR)
+    print(photars)
 
     import matplotlib.pyplot as plt
-    energy_centers = (EAR[:-1] + EAR[1:]) / 2
+    energy_centers = (ear[:-1] + ear[1:]) / 2
     fig, ax = plt.subplots()
-    ax.plot(energy_centers, PHOTAR)
+    for i in range(params.shape[0]):
+        ax.plot(energy_centers, photars[i])
+    ax.plot(energy_centers, photar)
     ax.set_xscale("log")
     ax.set_yscale("log")
     plt.show()
--- xcomptf/components/diskpbb_tf.py
+++ xcomptf/components/diskpbb_tf.py
@@ -9,6 +9,7 @@ and thus, sometimes output wrong result from the original ones.
 import tensorflow as tf
 
 
+@tf.function(autograph=False, jit_compile=True)
 def dkbflx(tin, photon_index, energy, dtype=tf.float32):
     """
     calculate the flux of the disk blackbody model at a given energy.
@@ -22,14 +23,25 @@ def dkbflx(tin, photon_index, energy, dtype=tf.float32):
     returns:
         float: photon flux in photons/s/cm^2/kev.
     """
-    batch_shape = tin.shape
-
     gauss = tf.constant(
         [0.236926885, 0.478628670, 0.568888888, 0.478628670,
          0.236926885, -0.906179846, -0.538469310, 0.0,
          0.538469310, 0.906179846], dtype=dtype)
 
-    nnn = 1000
+    # nnn determines the accuracy of the numerical integration
+    # and the time of the calculation.
+    # When the minimum enegy is too low, it sometimes raise
+    # memory allocation error. Consider employ adapting `nnn`
+    # depending on energy and tin.
+    energy_min = tf.reduce_min(energy)
+    tin_min = tf.reduce_min(tin)
+    nnn = 10000
+    nnn = tf.cond(energy_min > 0.001 * tin_min, lambda: 1000, lambda: nnn)
+    nnn = tf.cond(energy_min > 0.01 * tin_min, lambda: 500, lambda: nnn)
+    nnn = tf.cond(energy_min > 0.1 * tin_min, lambda: 100, lambda: nnn)
+    nnn = tf.cond(energy_min > 0.2 * tin_min, lambda: 10, lambda: nnn)
+    nnn = tf.cond(energy_min > tin_min, lambda: 5, lambda: nnn)
+
     xn = 1.0 / tf.cast(nnn, dtype) / 2.0
 
     gauss_left = gauss[0:5]
@@ -38,12 +50,10 @@ def dkbflx(tin, photon_index, energy, dtype=tf.float32):
     xh = xn * (2. * tf.range(1, nnn+1, 1, dtype=dtype) - 1)
     x = xn * gauss_right[tf.newaxis, :] + xh[..., tf.newaxis]
 
-    assert energy.shape == (3491, 5)
     energy_integ = energy[tf.newaxis, ..., tf.newaxis, tf.newaxis]
     tin_integ = tin[..., tf.newaxis, tf.newaxis, tf.newaxis, tf.newaxis]
     photon_index_integ = photon_index[..., tf.newaxis, tf.newaxis,
                                       tf.newaxis, tf.newaxis]
-    assert energy_integ.shape == (1, 3491, 5, 1, 1)
     dk = tf.where(
         energy_integ / tin_integ / x >= 170,
         x ** (-2.0 / photon_index_integ - 1.0) * tf.exp(
@@ -58,17 +68,19 @@ def dkbflx(tin, photon_index, energy, dtype=tf.float32):
     photar = 2.78e-3 * energy_batch * energy_batch * photon * (
         0.75 / photon_index_batch)
 
-    assert x.shape == (nnn, 5)
-    assert energy.shape == (3491, 5)
-    assert dk.shape == (*batch_shape, 3491, 5, nnn, 5)
-    assert gauss_left.shape == (5,)
-    assert photon.shape == (*batch_shape, 3491, 5)
-    assert photar.shape == (*batch_shape, 3491, 5)
+    # assert energy.shape == (3491, 5)
+    # assert energy_integ.shape == (1, 3491, 5, 1, 1)
+    # assert x.shape == (nnn, 5)
+    # assert energy.shape == (3491, 5)
+    # assert dk.shape == (*batch_shape, 3491, 5, nnn, 5)
+    # assert gauss_left.shape == (5,)
+    # assert photon.shape == (*batch_shape, 3491, 5)
+    # assert photar.shape == (*batch_shape, 3491, 5)
 
     return photar
 
 
-# @tf.function(autograph=False, jit_compile=True)
+@tf.function(autograph=False, jit_compile=True)
 def diskpbb(ear, ne, param, ifl, dtype=tf.float32):
     """
     compute the multicolor disk blackbody model spectrum.
@@ -120,12 +132,16 @@ def main():
         [[0.2, 0.75],
          [0.1, 0.75]])
 
+    param = tf.tile([[0.2, 0.75]], (10000, 1))
+    param = tf.random.normal(shape=[10000, 2], mean=[0.2, 0.75],
+                             stddev=[0.01, 0.0], seed=1)
+    print("parameter shape is {}".format(param.shape))
+
     import time
     t0 = time.time()
     photar, photer = diskpbb(ear, ne, param, ifl)
     t1 = time.time()
     print("ran time is {} s".format(t1-t0))
-    print(photar)
 
     import matplotlib.pyplot as plt
     energy_centers = (ear[:-1] + ear[1:]) / 2
